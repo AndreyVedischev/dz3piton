11d1efcd17841365e388bb5361482968497593e0
Andrey <vedischev76@gmail.com>
2022-09-13 20:42:45 +0500

    updated task2,3

--- task2.py
+++ task2.py
@@ -4,3 +4,33 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+from random import randint
+
+n = int(input('Введите число элементов списка: '))
+x_min = int(input('Введите диапазон min: '))
+x_max = int(input('Введите диапазон max: '))
+
+n_list = []
+
+def get_random_number(list, len, min, max):
+    rnd = randint    
+    for i in range(0, len):
+        number = rnd(min, max)
+        list.append(number)
+    print(list)
+
+def mult_couple_elements(lst):
+    if len(lst) % 2 != 0:
+        length = len(lst)//2 + 1 
+    else:
+        length = len(lst)//2
+    new_lst = []
+    for i in range(length):
+        new_lst.append(lst[i]*lst[len(lst)-i-1])
+        i+=1
+    print(new_lst)
+
+
+get_random_number(n_list, n, x_min, x_max)
+mult_couple_elements(n_list)
